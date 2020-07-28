73f8e863115e3510638a2cd4765beeacdd295909
pkmishra <pkmishra@gmail.com>
2020-07-28 12:41:44 -0700

    finished project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,60 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// While any key is pressed make screen black
+// As soon as key is released make it white
+// 1 row = 16 X 32 = 512 pixel - total 16 rows
+//Each row in the physical screen,starting at the screen’s top left corner, is represented in the RAM by 32 consecu-tive 16-bit words
+
+@8192
+D=A
+@max
+M=D
+
+(LOOP)
+  @0
+  D=A
+  @i
+  M=D
+
+(SCREENLOOP)
+  @i
+  M=M+1
+  //Reset iteration if needed
+  D=M
+  @max
+  D=M-D
+  @LOOP
+  D;JEQ
+
+  @KBD
+  D=M
+  @DARK
+  D;JNE //Draw Black Pixels If A key Was Pressed
+  @LIGHT
+  D;JEQ
+
+
+(DARK)
+  @i
+  D=M
+  @SCREEN
+  A=A+D
+  M=-1
+  @SCREENLOOP
+  0;JMP
+
+(LIGHT)
+  @i
+  D=M  //current pointer to i
+  @SCREEN
+  A=A+D
+  M=0
+  @SCREENLOOP
+  0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,35 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// Sum R0 , R1 times and store the result in R2
+// sum = 0
+// for (i = 0; i< R1;i++):
+//    sum = sum + R0
+// R2 = sum
+
+  @i
+  M=0
+  @R2
+  M=0
+
+(LOOP)
+  @i
+  D=M //D=i
+  @R1
+  D=D-M // D = i - R1
+  @END
+  D;JGE // i-R1 > 0 go to end
+  @R0
+  D=M //D = R0
+  @R2
+  M=D+M // Sum =  R0 + Sum
+  @i
+  M=M+1
+  @LOOP
+  0;JMP //Go to loop
+
+
+
+(END)
+  @END
+  0;JMP //Infinite loop
